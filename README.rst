f9a81beb8821b65153a0aefabe11d082f64ddb92
Aristotelis <amikrop@gmail.com>
2015-01-12 21:44:54 +0200

    Typo

--- README.rst
+++ README.rst
@@ -8,7 +8,7 @@ A poker engine core, in Python
 *Card* (a playing card), *Hand* (a poker hand consisting of Cards,
 with evaluation capabilities) and *Deck* (a deck of Cards).
 
-Its is a simple starter, mainly written for exploring purposes,
+It is a simple starter, mainly written for exploring purposes,
 but can be extended and/or used to build something bigger. It is
 released under the MIT licensre.
 
--- pokercore/__init__.py
+++ pokercore/__init__.py
@@ -6,7 +6,7 @@ pokercore provides 3 classes to be used in a poker engine.
 Card (a playing card), Hand (a poker hand consisting of Cards,
 with evaluation capabilities) and Deck (a deck of Cards).
 
-Its is a simple starter, mainly written for exploring purposes,
+It is a simple starter, mainly written for exploring purposes,
 but can be extended and/or used to build something bigger. It is
 released under the MIT license.
 
